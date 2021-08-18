7ed59573e988aeb9d645868f7ab7d3b55ba649d7
Kishan Aghera <55836816+kishan-aghera@users.noreply.github.com>
2021-08-18 11:45:20 +0530

    Added Question model

---
+++ app/questions/mutations/updateQuestion.ts
@@ -0,0 +1,20 @@
+import { resolver } from "blitz"
+import db from "db"
+import { z } from "zod"
+
+const UpdateQuestion = z.object({
+  id: z.number(),
+  text: z.string(),
+})
+
+export default resolver.pipe(
+  resolver.zod(UpdateQuestion),
+  resolver.authorize(),
+  async ({ id, ...data }) => {
+    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
+    await db.choice.deleteMany({ where: { questionId: id } })
+    const question = await db.question.update({ where: { id }, data })
+
+    return question
+  }
+)
